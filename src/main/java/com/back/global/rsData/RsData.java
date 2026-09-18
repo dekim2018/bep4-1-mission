a9024476f8aab098f7e62ab765c725f3ef368a1d
dekim2018 <de2018kim@gmail.com>
2026-09-18 16:18:23 +0900

    0023

--- src/main/java/com/back/global/rsData/RsData.java
+++ src/main/java/com/back/global/rsData/RsData.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class RsData<T> {
-    private final String resultCode;
+    private final String resultCode; //200-1 성공 등(200은 http 상태코드, -1은 케이스별 회사규칙)
     private final String msg;
     private final T data;
 
